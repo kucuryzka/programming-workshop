b7f781926462462d2c511a7f2386fb4d3a7dd5ff
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2025-03-09 18:31:36 +0600

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,5 +1,6 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-double* finding_roots_quadratic_equation(double a, double b, double c, double eps);
+double *finding_roots_quadratic_equation(double a, double b, double c,
+                                         double eps);
