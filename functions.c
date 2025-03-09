72ebf86c9bde9bb78a2f307250d17cfc2b43de2c
KaPie2 <117990421+KaPie2@users.noreply.github.com>
2025-03-09 17:21:54 +0600

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,23 @@
+#include "functions.h"
+
+double* finding_roots_quadratic_equation(double a, double b, double c, double eps) {
+    // корней нет изначально
+    double* roots = NULL;
+
+    if (fabs(a) < eps) {
+        return roots;
+    }
+    double d = b * b - 4 * a * c;
+
+    if (d > eps) { // Дискриминант больше нуля
+        roots = malloc(2 * sizeof(double));
+        roots[1] = (-b + sqrt(d)) / (2 * a);
+        roots[0] = c / (a * roots[1]);
+    } else if (d < -eps) { // Дискриминант меньше нуля
+        return roots;
+    } else { // Дискриминант равен нулю
+        roots = malloc(sizeof(double));
+        roots[0] = -b / (2 * a);
+    }
+    return roots;
+}
