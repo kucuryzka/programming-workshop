b9fbd06d6602f0c703789cbf4cae411d0d478f00
olesya <olegha2000@gmail.com>
2025-04-10 13:58:12 +0600

    fix: fixed format

--- linear_allocator.c
+++ linear_allocator.c
@@ -2,29 +2,27 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+void AllocInit(LinearAllocator *alloc, size_t size) {
+  alloc->pointer = malloc(size);
 
-void AllocInit(LinearAllocator *alloc, size_t size){
-    alloc->pointer = malloc(size);
-    
-    alloc->total_size = size;
-    alloc->current = 0;
+  alloc->total_size = size;
+  alloc->current = 0;
 }
 
+void *Allocate(LinearAllocator *alloc, size_t size) {
+  if (alloc->current + size > alloc->total_size)
+    return NULL;
 
-void* Allocate(LinearAllocator *alloc, size_t size){
-    if (alloc->current + size > alloc->total_size) return NULL;
+  void *pointer = alloc->pointer + alloc->current;
+  alloc->current += size;
 
-    void* pointer = alloc->pointer + alloc->current;
-    alloc->current += size;
-
-    return pointer;
+  return pointer;
 }
 
+void Free(LinearAllocator *alloc) {
+  free(alloc->pointer);
 
-void Free(LinearAllocator *alloc){
-    free(alloc->pointer);
-
-    alloc->pointer = NULL;
-    alloc->total_size = 0;
-    alloc->current = 0;
+  alloc->pointer = NULL;
+  alloc->total_size = 0;
+  alloc->current = 0;
 }
--- linear_allocator.h
+++ linear_allocator.h
@@ -1,15 +1,13 @@
 #include <stddef.h>
 
-
-typedef struct{
-    size_t total_size;
-    char* pointer;
-    size_t current;
-}LinearAllocator;
-
+typedef struct {
+  size_t total_size;
+  char *pointer;
+  size_t current;
+} LinearAllocator;
 
 void AllocInit(LinearAllocator *alloc, size_t size);
 
-void* Allocate(LinearAllocator *alloc, size_t size);
+void *Allocate(LinearAllocator *alloc, size_t size);
 
 void Free(LinearAllocator *alloc);
